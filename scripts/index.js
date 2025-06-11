abc66a724ee601d35b01102147ef044e17b3f671
Valen-tiina <vale.rodriguez.palacios@gmail.com>
2025-06-11 08:48:59 -0500

    arreglos en el menu 11-06

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+    
+})
+//visibilidad del boton al hacer click
  const btn = document.getElementById("btnMenu");
     const sideMenu = document.getElementById("sideMenu");
 
     btn.addEventListener("click", () => {
       sideMenu.classList.toggle("open");
+    });
+
+//quitar el menu al hacer scroll
+    window.addEventListener("scroll", function(){
+      if(window.scrollY > 0) {
+        sideMenu.classList.remove("open");
+      }
     });
