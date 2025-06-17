c3d9a7932b5e5ad380023ec2feae86b63baaf7db
valen-tiina <vale.rodriguez.palacios@gmail.com>
2025-06-17 09:11:10 -0500

    actualizaciones 17-06

--- scripts/index.js
+++ scripts/index.js
@@ -17,3 +17,33 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 
+//slider - section carrusel
+const slides = document.getElementById("contentsCarrusel");
+  const totalSlides = slides.children.length;
+  const slideWidth = slides.children[0].offsetWidth;
+
+  let currentIndex = 0;
+
+  function slideTo(index, smooth = true) {
+    slides.style.transition = smooth ? "transform 0.5s ease-in-out" : "none";
+    slides.style.transform = `translateX(-${index * slideWidth}px)`;
+  }
+
+  function siguienteSlide() {
+    currentIndex++;
+    slideTo(currentIndex);
+
+    // volver sin animacion 
+    if (currentIndex === totalSlides - 1) {
+      setTimeout(() => {
+        currentIndex = 0;
+        slideTo(currentIndex, false); // sin animación
+      }, 500); // espera a que termine la transición
+    }
+  }
+
+  // Moverse automáticamente cada 4 segundos
+  setInterval(siguienteSlide, 4000);
+
+
+
